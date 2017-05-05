702e14020ad1f64017053d870b4d6f32d5ac6017
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-05 16:35:14 -0700

    crawler search for word

--- crawler.js
+++ crawler.js
@@ -1,3 +1,28 @@
 var request = require('request');
-var cheerio = require('cherio');
+var cheerio = require('cheerio');
 var URL = require('url-parse');
+
+var pageToVisit = "http://www.arstechnica.com";
+console.log("Visiting page " + pageToVisit);
+request(pageToVisit, function(error, response, body) {
+   if(error) {
+     console.log("Error: " + error);
+   }
+   // Check status code (200 is HTTP OK)
+   console.log("Status code: " + response.statusCode);
+   if(response.statusCode === 200) {
+     // Parse the document body
+     var $ = cheerio.load(body);
+     console.log("Page title:  " + $('title').text());
+    //  console.log($('body').text());
+     console.log(searchForWord($, "science"))
+   }
+});
+
+function searchForWord($, word) {
+  var bodyText = $('html > body').text();
+  if(bodyText.toLowerCase().indexOf(word.toLowerCase()) !== -1) {
+    return bodyText.toLowerCase().indexOf(word.toLowerCase())
+  }
+  return false;
+}
